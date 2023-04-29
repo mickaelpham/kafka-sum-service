10c4d164863250efcb550252676ed9bdfdede123
Mickaël Pham <inbox@mickael.dev>
2023-04-29 12:30:40 +0200

    Partition services

--- create-topics.sh
+++ create-topics.sh
@@ -1,2 +1,2 @@
-docker compose exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic sum-requests
-docker compose exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic sum-replies
+docker compose exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic sum-requests --partitions 8
+docker compose exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic sum-replies --partitions 8
--- src/sum-service.ts
+++ src/sum-service.ts
@@ -14,6 +14,8 @@ const SUM_REQUEST_TOPIC = 'sum-requests';
 const producer = kafka.producer();
 const consumer = kafka.consumer({ groupId: 'sum-service-consumer' });
 
+let messagesHandledCount = 0;
+
 const run = async () => {
   await producer.connect();
   await consumer.connect();
@@ -23,6 +25,8 @@ const run = async () => {
 
   await consumer.run({
     eachMessage: async ({ message: { headers, value } }) => {
+      messagesHandledCount += 1;
+
       // headers validation
       const { correlationId, replyTo } = parseHeaders(headers);
       if (!correlationId) {
@@ -65,6 +69,7 @@ const run = async () => {
 
   // set a timeout to disconnect both producer and consumer
   setTimeout(() => {
+    console.log(`sum service handled ${messagesHandledCount} messages`);
     console.log('disconnecting producer and consumer');
 
     producer
